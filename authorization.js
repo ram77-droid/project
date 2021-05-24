5e0937134f7afc43066a182b10eb5ad96f7c4f4d
ram77-droid <ramm00324@gmail.com>
2021-05-24 11:31:38 +0530

    change

--- authorization.js
+++ authorization.js
@@ -10,7 +10,7 @@ module.exports.check= function check(req,res,next)
     {
         token=req.headers.authorization.split(' ')[1];
         var vary=jwt.verify(token,'ram');
-        user.users.findOne({token:token},function(err,result){
+        user.users.findOne({token:vary.token},function(err,result){
             if(err)
             {
                 return res.json({
