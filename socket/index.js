a28d51a83b768a42d8e11314ff6884004d43a230
ram77-droid <ramm00324@gmail.com>
2021-07-12 14:11:44 +0530

    change

--- socket/index.js
+++ socket/index.js
@@ -15,28 +15,30 @@ io.on('connection', function (socket) {
             console.log('disconnected'); 
         }); 
     socket.on('join', function (req) {
-   // socket.join(req.user_id);
+    socket.join(req.user_id);
+    console.log("reqq",req.user_id);
+    io.sockets.in(req.user_id).emit("join" , { status: 1 , message: "Sucessfully Joined."});
     
-        if(req.user_id)
-        {
-            console.log("user:",req.user_id);
-            user.users.findOne({_id:req.user_id},function(err,result){
-                if(err)
-                {
-                    return err;
-                }
-                else if(result)
-                {
-                    console.log("join");
-                    console.log("reqqq",req.user_id);
-                    io.sockets.emit('join', { status: 1 , message: "Sucessfully Joined."});
-                }
-                else
-                {
-                    io.sockets.emit('join', { status: 1 , message:"something went wrong" });
-                }
-            });
-            } 
+        // if(req.user_id)
+        // {
+        //     console.log("user:",req.user_id);
+        //     user.users.findOne({_id:req.user_id},function(err,result){
+        //         if(err)
+        //         {
+        //             return err;
+        //         }
+        //         else if(result)
+        //         {
+        //             console.log("join");
+        //             console.log("reqqq",req.user_id);
+        //             io.sockets.emit('join', { status: 1 , message: "Sucessfully Joined."});
+        //         }
+        //         else
+        //         {
+        //             io.sockets.emit('join', { status: 1 , message:"something went wrong" });
+        //         }
+        //     });
+        //     } 
         });   
 
         socket.on('initialize',function(req){
@@ -78,10 +80,7 @@ io.on('connection', function (socket) {
         });
 
         socket.on('viewmessage',function(req){
-        if(req.user_id)
-        {
-        
-            user.message.find({receiver_id:req.user_id},function(err,result)
+            user.message.find({$or:[{receiver_id:req.receiver_id},{sender_id:req.sender_id}]},function(err,result)
             {
                 if(err)
                 {
@@ -91,7 +90,7 @@ io.on('connection', function (socket) {
                 {
 
                     console.log("res:",result[0].receiver_id);
-                    if(req.user_id==result[0].receiver_id)
+                    if(req.receiver_id==result[0].receiver_id || req.sender_id==result[0].sender_id)
                     {
 
                     
@@ -105,35 +104,75 @@ io.on('connection', function (socket) {
                 {
                     io.sockets.emit('viewmessage',{status:1,message:"not good"});
                 }
-            }).sort({message:1});
-        }
+            }).sort({message:-1});
+        // if(req.receiver_id || req.sender_id)
+        // {
+        
+            
+        // }
         });
 
     socket.on('sendmessage',function(req){
         console.log("req:",req);
+        console.log("ttype",typeof(req));
+        console.log("chat id",req.chat_id);
         // io.sockets.emit('mess',{status:1,message:"mess"});
-        obj={
-            chat_id:req.chat_id,
-            sender_id:req.sender_id,
-            receiver_id:req.receiver_id,
-            message:req.message
-        }
-        console.log("object:",obj);
-        user.message.create(obj,function(err,result){
+       
+        user.chat.findOne({_id:req.chat_id},function(err,result){
+            
             if(err)
             {
-                console.log('err:',err);
+                io.sockets.emit('sendmessage',{status:1,message:err.message});
             }
             else if(result)
             {
-                console.log("resulttt",result);
-                io.sockets.emit('sendmessage',{status:1,message:"message sent"});
-            }
+                console.log("result is",result.sender_id);
+                console.log("result is",result.receiver_id);
+                console.log("req sender id is",req.sender_id);
+                console.log("req receiver id is",req.receiver_id);
+                if(result.sender_id==req.sender_id||result.sender_id==req.receiver_id && result.receiver_id==req.sender_id||result.receiver_id==req.receiver_id)
+                {
+                    obj={
+                            chat_id:req.chat_id,
+                            sender_id:req.sender_id,
+                            receiver_id:req.receiver_id,
+                            message:req.message
+                        }
+
+                    console.log("object:",obj);
+                    user.message.create(obj,function(err,result){
+                        if(err)
+                        {
+                            io.sockets.emit('sendmessage',{status:1,message:err.message});
+                        }
+                        else if(result)
+                        {
+                            console.log("resulttt",result.message);
+                            io.sockets.emit('sendmessage',{status:1,message:"message sent"});
+                            io.sockets.in(req.receiver_id).emit('viewmessage',result.message);
+                            io.sockets.in(req.sender_id).emit('viewmessage',result.message);
+                        }
+                        else
+                        {
+                            console.log("something went wrong");
+                        }
+                    });
+
+                }
+                else
+                {
+                    console.log("error");
+                    io.sockets.in(req.receiver_id).emit('sendmessage',{status:1,message:"some thing went wrong!!..check the ids"});
+                    io.sockets.in(req.sender_id).emit('sendmessage',{status:1,message:"some thing went wrong!!..check the ids"});
+
+                }             
+            }  
             else
             {
-                console.log("something went wrong");
+                console.log("soemthing went wrong");
             }
         });
+       
     });
 });
 
